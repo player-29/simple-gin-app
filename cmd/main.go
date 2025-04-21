4b4900a1ae085b7108432704e7dbf0b87e84c68b
ayushrathour <ayushrathour2910@gmail.com>
2025-04-21 15:00:30 +0530

    update

--- cmd/main.go
+++ cmd/main.go
@@ -20,7 +20,7 @@ func PingHandler(c *gin.Context) {
 func main() {
 
 	r := gin.Default()
-	r.GET("/pinging", PingHandler)
+	r.GET("/ping", PingHandler)
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
